ad71f753f4eba850b0ec5868050a5a517124e7cd
ernesto <ernesto002@icloud.com>
2021-12-02 16:51:22 -0800

    explain functionality

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+// Initialize "current-count" within JavaScript to 0
 let count = 0
+
+// currentCount variable grabs the "current-count" element from the DOM
 let currentCount = document.getElementById("current-count")
+
+// save variable grabs the "previous-customers" from the DOM
 let save = document.getElementById("previous-customers")
 
-
+// addCustomer function adds 1 to the customer count each time 
+// the "customer-bttn" button is clicked
 function addCustomer() {
     count += 1
     currentCount.textContent = count
 }
 
+// saveCount function renders the "current-count" to the "previous-customers" 
+// paragraph while resetting the "current-count" to 0 when clicked
 function saveCount() {
     let previousEntries = count + " - "
     save.textContent += previousEntries
